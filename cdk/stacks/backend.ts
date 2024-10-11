54aec011aa96431f01fbffaf013e8eafb75acbf2
Chris Lasher <chris.lasher@gmail.com>
2024-10-11 18:59:56 -0400

    Grant the lambda access to the cookie secret.

--- cdk/stacks/backend.ts
+++ cdk/stacks/backend.ts
@@ -1,5 +1,6 @@
 import * as cdk from "aws-cdk-lib";
 import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as ssm from "aws-cdk-lib/aws-ssm";
 import type { Construct } from "constructs";
 
 interface BackendStackProps extends cdk.StackProps {
@@ -12,6 +13,18 @@ export class BackendStack extends cdk.Stack {
 
   constructor(scope: Construct, id: string, props: BackendStackProps) {
     super(scope, id, props);
+    const cookieSecret =
+      ssm.StringParameter.fromSecureStringParameterAttributes(
+        this,
+        "RemixCookieSecret",
+        {
+          parameterName: "/remix/cookie-secret",
+          version: 1,
+        },
+      );
+    const paramsAndSecrets = lambda.ParamsAndSecretsLayerVersion.fromVersion(
+      lambda.ParamsAndSecretsVersions.V1_0_103,
+    );
 
     const backendLambda = new lambda.Function(this, "RemixBackendLambda", {
       runtime: lambda.Runtime.NODEJS_20_X,
@@ -23,7 +36,10 @@ export class BackendStack extends cdk.Stack {
         COGNITO_USER_POOL_ID: props.cognitoUserPoolId,
         COGNITO_CLIENT_ID: props.cognitoUserPoolClientId,
       },
+      paramsAndSecrets,
     });
+
+    cookieSecret.grantRead(backendLambda);
     this.lambdaURL = backendLambda.addFunctionUrl({
       authType: lambda.FunctionUrlAuthType.NONE,
     });
