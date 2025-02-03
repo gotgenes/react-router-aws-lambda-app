cc712728bbf98e96cf865a73aea5d9631ea617f9
Chris Lasher <chris.lasher@gmail.com>
2025-02-03 10:31:47 -0500

    Add most v3 feature flags.

--- site/vite.config.ts
+++ site/vite.config.ts
@@ -2,9 +2,24 @@ import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
+declare module "@remix-run/node" {
+  interface Future {
+    v3_singleFetch: true;
+  }
+}
+
 export default defineConfig({
   plugins: [
-    remix(),
+    remix({
+      future: {
+        unstable_optimizeDeps: true,
+        v3_fetcherPersist: true,
+        v3_relativeSplatPath: true,
+        v3_throwAbortReason: true,
+        v3_lazyRouteDiscovery: true,
+        v3_singleFetch: true,
+      },
+    }),
     tsconfigPaths(),
     {
       name: "remix-apigatewayv2-adapter",
