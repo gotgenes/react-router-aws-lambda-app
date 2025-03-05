e4d6da8d9b9bc932b47482942fde0acc9910d8f0
Chris Lasher <chris.lasher@gmail.com>
2025-03-05 11:01:53 -0500

    Use react-router-serve to run the Lambda.

--- site/scripts/build.sh
+++ site/scripts/build.sh
@@ -2,13 +2,14 @@
 
 set -e
 
+# Set the CWD to the site directory
 cd "$(dirname "$0")/.."
 
 # Ensure tooling is installed
 npm install
 # Build the client and server bundles
-npx react-router build
-cp package.json run.sh server.js build/server/
+NODE_ENV=production npx react-router build
+cp package.json run.sh build/server/
 
 # Set the CWD to the project root directory
 cd "$(git rev-parse --show-toplevel)"
@@ -18,4 +19,4 @@ npm ci --omit dev --workspace=site
 find node_modules -depth 1 -type l -exec rm {} \;
 cp -R node_modules site/build/server/
 # Re-install tooling
-npm install
+npm ci
--- site/vite.config.ts
+++ site/vite.config.ts
@@ -4,18 +4,5 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-    reactRouter(),
-    tsconfigPaths(),
-    {
-      name: "react-router-lambda-web-adapter",
-      apply(_config, env): boolean {
-        return env.command === "build" && env?.isSsrBuild === true;
-      },
-      config: async () => {
-        return { build: { ssr: "app.ts" } };
-      },
-    },
-  ],
+  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
 });
