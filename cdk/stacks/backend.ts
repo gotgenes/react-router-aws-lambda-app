0b26d9df470dd6f53ab2357d04e30733ede9ecd5
Chris Lasher <chris.lasher@gmail.com>
2025-03-06 16:32:40 -0500

    Use the Docker image for a new Lambda.

--- cdk/stacks/backend.ts
+++ cdk/stacks/backend.ts
@@ -1,6 +1,9 @@
 import * as cdk from "aws-cdk-lib";
+import { Platform } from "aws-cdk-lib/aws-ecr-assets";
 import * as lambda from "aws-cdk-lib/aws-lambda";
 import type { Construct } from "constructs";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 
 export class BackendStack extends cdk.Stack {
   public readonly lambdaURL: lambda.FunctionUrl;
@@ -31,8 +34,36 @@ export class BackendStack extends cdk.Stack {
         },
       },
     );
+    const lambdaURL = backendLambda.addFunctionUrl({
+      authType: lambda.FunctionUrlAuthType.NONE,
+      invokeMode: lambda.InvokeMode.RESPONSE_STREAM,
+    });
+    this.exportValue(lambdaURL.url);
 
-    this.lambdaURL = backendLambda.addFunctionUrl({
+    const image = lambda.DockerImageCode.fromImageAsset(
+      path.join(path.dirname(fileURLToPath(import.meta.url)), "..", ".."),
+      {
+        file: path.join("site", "Dockerfile"),
+        platform: Platform.LINUX_ARM64,
+      },
+    );
+    const dockerBackendLambda = new lambda.DockerImageFunction(
+      this,
+      "ReactRouterDockerBackendLambda",
+      {
+        code: image,
+        architecture: lambda.Architecture.ARM_64,
+        environment: {
+          NODE_ENV: "production",
+          PORT: "8080",
+          AWS_LAMBDA_EXEC_WRAPPER: "/opt/bootstrap",
+          AWS_LWA_ENABLE_COMPRESSION: "true",
+          AWS_LWA_INVOKE_MODE: "response_stream",
+        },
+      },
+    );
+
+    this.lambdaURL = dockerBackendLambda.addFunctionUrl({
       authType: lambda.FunctionUrlAuthType.NONE,
       invokeMode: lambda.InvokeMode.RESPONSE_STREAM,
     });
