6b57e07d7fdd67052e5a179a23f2a64f7387cf9e
Chris Lasher <chris.lasher@gmail.com>
2025-03-14 16:14:23 -0400

    fix(dev): Get AWS region from profile, if unset.

--- scripts/docker-login.sh
+++ scripts/docker-login.sh
@@ -5,5 +5,6 @@ set -e
 set -x
 
 ecr_uri=$(aws ecr describe-repositories --output text --query 'repositories[?repositoryName==`react-router`] | [0].repositoryUri')
+region="${CDK_DEFAULT_REGION:-$(aws configure get region)}"
 registry_url=${ecr_uri%/*}
-aws ecr get-login-password --region "$CDK_DEFAULT_REGION" | docker login --username AWS --password-stdin "$registry_url"
+aws ecr get-login-password --region "$region" | docker login --username AWS --password-stdin "$registry_url"
