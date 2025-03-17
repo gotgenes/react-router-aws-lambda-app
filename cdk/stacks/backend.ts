9e6f6b3535e280056cc7e5618c9500fa4a0d1e85
Chris Lasher <chris.lasher@gmail.com>
2025-03-17 15:58:19 -0400

    fix(cdk): Update image digest at deployment time.

--- cdk/stacks/backend.ts
+++ cdk/stacks/backend.ts
@@ -15,16 +15,15 @@ export class BackendStack extends cdk.Stack {
     super(scope, id, props);
     const repository = props.containerRepository;
 
-    const imageDigest = ssm.StringParameter.fromStringParameterName(
+    const imageDigest = ssm.StringParameter.valueForStringParameter(
       this,
-      "ReactRouterBackendImageID",
       "/react-router/image-digest",
     );
     // Workaround for incorrect parameter expansion
     // https://github.com/aws/aws-cdk/issues/20213#issuecomment-1954682333
     const parsedImageID = cdk.Fn.select(
       1,
-      cdk.Fn.split("sha256:", imageDigest.stringValue),
+      cdk.Fn.split("sha256:", imageDigest),
     );
     const image = lambda.DockerImageCode.fromEcr(repository, {
       tagOrDigest: `sha256:${parsedImageID}`,
