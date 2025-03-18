e4bb13c10bd0dbe1c4da8cfbcfc842f3d49a4dd6
Chris Lasher <chris.lasher@gmail.com>
2025-03-17 22:53:22 -0400

    build: Use buildx bake.
    This lets users build with Docker Desktop with containerd support.

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,5 +3,8 @@ set -e
 set -x
 
 npm run build --workspace=site
-docker compose build
+# Lambda doesn't support provenance attestations and multi-platform builds
+# https://github.com/aws/aws-cdk/issues/30258
+# https://github.com/aws/aws-cdk/issues/31548
+docker buildx bake --provenance=false
 npm run build --workspace=cdk
