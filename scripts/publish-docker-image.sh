291f200cd8ee119186cf13ea19d764a0447801ba
Chris Lasher <chris.lasher@gmail.com>
2025-03-27 09:38:21 -0400

    build: Extract more variables.

--- scripts/publish-docker-image.sh
+++ scripts/publish-docker-image.sh
@@ -5,14 +5,16 @@ set -e
 set -x
 
 REPOSITORY_NAME="react-router"
+LOCAL_IMAGE_NAME="react-router-aws-lambda-lambda"
 IMAGE_PARAMETER_NAME="/react-router/image-digest"
 APP_TAG="ReactRouterApp"
 
 query="repositories[?repositoryName==\`$REPOSITORY_NAME\`] | [0].repositoryUri"
 ecr_uri=$(aws ecr describe-repositories --output text --query "$query")
-docker tag react-router-aws-lambda-lambda:latest "$ecr_uri"
-docker push "$ecr_uri:latest"
-image_digest=$(docker inspect --format='{{range .RepoDigests}}{{println .}}{{end}}' "${ecr_uri}:latest" | grep amazonaws.com | cut -d '@' -f 2)
+ecr_tag="$ecr_uri:latest"
+docker tag "$LOCAL_IMAGE_NAME:latest" "$ecr_uri"
+docker push "$ecr_tag"
+image_digest=$(docker inspect --format='{{range .RepoDigests}}{{println .}}{{end}}' "$ecr_tag" | grep amazonaws.com | cut -d '@' -f 2)
 
 if current_parameter_value="$(aws ssm get-parameter --output text --name "$IMAGE_PARAMETER_NAME" --query Parameter.Value 2>/dev/null)"; then
     if [[ $image_digest != $current_parameter_value ]]; then
